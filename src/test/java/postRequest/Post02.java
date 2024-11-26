cbd34fdacb01f9d2d748c3bce0d75d3f8ed74fdc
muhammetsdet <atasdet33@gmail.com>
2024-11-27 00:02:25 +0300

    27.11.2024

--- src/test/java/postRequest/Post02.java
+++ src/test/java/postRequest/Post02.java
@@ -1,49 +1,78 @@
 package postRequest;
 
+import baseURL.DummyRestApiBaseURL;
 import baseURL.GoRestApiBaseURL;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.junit.Test;
+import pojoDatas.DummyExpectedDataPojo;
+import pojoDatas.DummyRestApiPojo;
 import testData.GoRestApiTestData;
 
 import static io.restassured.RestAssured.given;
 import static org.junit.Assert.assertEquals;
 
-public class Post02 extends GoRestApiBaseURL {
+public class Post02 extends DummyRestApiBaseURL {
 
 /*
-        Given
-            https://gorest.co.in/public/v2/users
-        When
-            Kullanıcı Post Methodu ile Request Gönderir
-        Then
-            Status Code'un "422" olduğunu Assert et
+
+   Given
+
+        //http://dummy.restapiexample.com/api/v1/create
+    And
+        Request Body olarak aşağıdaki pattern de body gönderiniz
+      {
+  "name": "Kemal Sunal",
+  "salary": "40000",
+  "age": "27"
+}
+    When
+        Kullanıcı POST Methodu ile Request Gönderir
+    Then
+        Status Code un "200" olduğunu Assert et
+    And
+           Response Body nin aşağıdaki şekilde olduğunu doğrulayınız
+           {
+    "status": "success",
+    "data": {
+  "name": "Kemal Sunal",
+  "salary": "40000",
+  "age": "27",
+
+    },
+    "message": "Successfully! Record has been added."
+}
+*/
+@Test
+    public void post02(){
+    //Set the URL
+    specification.pathParams("createPath","create");
+//Set the expected data
+    DummyRestApiPojo dummyRestApiPojo = new DummyRestApiPojo("Kemal Sunal","40000","27");
+
+    DummyExpectedDataPojo dummyExpectedDataPojo = new DummyExpectedDataPojo("success",dummyRestApiPojo,"Successfully! Record has been added.");
+//Normalde Api Doc.  da bizden Basic Authorization istenmiyor ama syntax öğrenmek için biz yine de kullanacağız.
+//Send the request
+  Response response= given(specification)
+           .contentType(ContentType.JSON)
+           .auth().basic("admin","password1234")
+           .when()
+           .body(dummyRestApiPojo)
+          .post("/{createPath}");
+  response.prettyPrint();
+
+  //Do the assertion
+    DummyExpectedDataPojo actualData= response.as(DummyExpectedDataPojo.class);
+    System.out.println("actualData = " + actualData);
+
+    assertEquals(dummyExpectedDataPojo.getStatus(),actualData.getStatus());
+    assertEquals(dummyExpectedDataPojo.getMessage(),actualData.getMessage());
+    assertEquals(dummyExpectedDataPojo.getData().getAge(),actualData.getData().getAge());
+    assertEquals(dummyExpectedDataPojo.getData().getName(),actualData.getData().getName());
+    assertEquals(dummyExpectedDataPojo.getData().getSalary(),actualData.getData().getSalary());
 
 
- */
+}
 
-    @Test
-    public void post02() {
-        //Set the URL
-        specification.pathParams("usersPath", "users");
-
-        //Set the expected data
-        GoRestApiTestData GoRestApiTestData = new GoRestApiTestData();
-        System.out.println("GoRestApiTestData.statusCodeForInvalidPost() = " + GoRestApiTestData.statusCodeForInvalidPost());
-
-        //Send the request
-        Response response = given().spec(specification).when()
-                .header("Authorization", "Bearer 782b75699939a85ec01b26b0a015d718e7f1127a6856bf0ff3a8731f99fbc65a")
-                .post("/{usersPath}/{idPath");
-        response.prettyPrint();
-
-        //Assertion
-        assertEquals(GoRestApiTestData.statusCodeForInvalidPost(),response.statusCode());
-        response.then().assertThat().statusCode(GoRestApiTestData.statusCodeForInvalidPost());
-        response.then().assertThat().statusCode(422);
-        //-> 3 Farklı assert denenebilir
-
-        
-
-    }
 
 }
